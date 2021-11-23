3ab28cb1e9163e046351d0ccbc2da82c9c01a96f
Félix P <felix@saucelabs.com>
2021-11-23 22:59:23 +0100

    [DEVX-1363] Add support for Sauce report format (#2)
    * Add support for Sauce report format
    * Share code for screenshot

--- src/reporter.js
+++ src/reporter.js
@@ -1,4 +1,5 @@
 const SauceLabs = require('saucelabs').default;
+const {Status, TestRun, Suite, Attachment} = require('@saucelabs/sauce-json-reporter');
 const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
@@ -125,12 +126,63 @@ class Reporter {
 
         let assets = [
             this.createConsoleLog(session),
+            this.createSauceTestReport(session),
             ...this.getVideos(session.tests),
             ...this.getScreenshots(session.tests)];
 
         await this.uploadAssets(sessionId, assets);
 
         return this.getJobURL(sessionId);
+    }
+
+    createSauceTestReport(session) {
+        const testRun = new TestRun();
+        testRun.status = session.passed ? Status.Passed : Status.Failed;
+
+        let suite, fixture;
+
+        for (const test of session.tests) {
+            suite = testRun.withSuite(test.specPath);
+            fixture = suite.withSuite(test.fixtureName);
+
+            let duration;
+            if (test.startTime && test.endTime) {
+                duration = test.endTime - test.startTime;
+            }
+
+            const testStatus = (test.errs && test.errs.length) ? Status.Failed : Status.Passed;
+            suite.status = suite.status === Status.Passed ? testStatus : suite.status;
+            fixture.status = fixture.status === Status.Passed ? testStatus : fixture.status;
+
+            const addedTest = fixture.withTest(
+                test.name,
+                testStatus,
+                duration,
+                '',
+                test.startTime,
+                { browser: test.browser.name },
+            );
+
+            if (test.video) {
+                addedTest.attach({
+                    name: `${test.name}`,
+                    contentType: 'video/mp4',
+                    path: `${test.name}.mp4`,
+                });
+            }
+            for (const screenshot of test.screenshots) {
+                const filename = this.getScreenshotName(test.name, screenshot.screenshotPath);
+                addedTest.attach({
+                    name: filename,
+                    contentType: 'image/png',
+                    path: filename,
+                });
+            }
+        }
+        return {
+            filename: 'sauce-test-report.json',
+            data: testRun,
+        };
     }
 
     async createJob (body) {
@@ -223,15 +275,19 @@ class Reporter {
     getScreenshots (tests) {
         return tests.flatMap(test => {
             return test.screenshots.map(screenshot => {
-                // There can be multiple screenshots per test. Using a suffix to avoid collisions.
-                const suffix = path.basename(screenshot.screenshotPath);
                 return {
-                    filename: `${test.name} - ${suffix}`,
+                    filename: this.getScreenshotName(test.name, screenshot.screenshotPath),
                     data:     this.maybeReadFile(screenshot.screenshotPath)
                 };
             })
                 .filter(artifact => artifact.data);
         });
+    }
+
+    getScreenshotName (testName, filePath) {
+        // There can be multiple screenshots per test. Using a suffix to avoid collisions.
+        const suffix = path.basename(filePath);
+        return `${testName} - ${suffix}`;
     }
 
     maybeReadFile (filepath) {
