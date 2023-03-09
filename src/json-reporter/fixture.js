44bd96a5bcf085836c7019a9f76bce693639794b
Felix P <felix@saucelabs.com>
2023-03-09 11:14:15 +0100

    Fix coding style

--- src/json-reporter/fixture.js
+++ src/json-reporter/fixture.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 // @ts-check
+// eslint-disable-next-line no-unused-vars
 const { TestRun, Test } = require('@saucelabs/sauce-json-reporter');
 
 /**
@@ -7,121 +8,126 @@ const { TestRun, Test } = require('@saucelabs/sauce-json-reporter');
 * execution metadata (e.g. browser under test, generated assets).
 */
 class BrowserTestRun {
-  /**
-   * @param {string} userAgent
-   */
-  constructor(userAgent) {
-    this.userAgent = userAgent;
-    this.testRun = new TestRun();
-    this.testRun.metadata['userAgent'] = userAgent;
     /**
-     * @type {Array<{ name: string, localPath: string }>}
+     * @param {string} userAgent
      */
-    this.assets = [];
+    constructor(userAgent) {
+        this.userAgent = userAgent;
+        this.testRun = new TestRun();
+        // @ts-ignore
+        this.testRun.metadata['userAgent'] = userAgent;
+        /**
+         * @type {Array<{ name: string, localPath: string }>}
+         */
+        this.assets = [];
 
-    const [ browser, platform ] = userAgent.split('/').map((ua) => ua.trim());
+        const [ browser, platform ] = userAgent.split('/').map((ua) => ua.trim());
 
-    this._browser = browser;
-    this.platform = platform;
-  }
+        this._browser = browser;
+        this.platform = platform;
+    }
 
-  get browserName() {
-    const [ name = 'unknown',  _ ] = this._browser.split(' ');
+    get browserName() {
+        // eslint-disable-next-line no-unused-vars
+        const [ name = 'unknown',  _ ] = this._browser.split(' ');
 
-    return name;
-  }
+        return name;
+    }
 
-  get browserVersion() {
-    const [ _, version = 'unknown' ] = this._browser.split(' ');
+    get browserVersion() {
+        // eslint-disable-next-line no-unused-vars
+        const [ _, version = 'unknown' ] = this._browser.split(' ');
 
-    return version;
-  }
+        return version;
+    }
 
-  get browser() {
-    return this._browser;
-  }
+    get browser() {
+        return this._browser;
+    }
 
-  /**
-   * @param {string} fixturePath
-   * @param {string} fixtureName
-   * @param {Test} test
-   */
-  addTest(fixturePath, fixtureName, test) {
-    this.testRun.withSuite(fixturePath).withSuite(fixtureName).addTest(test);
-    this.testRun.computeStatus();
-  }
+    /**
+     * @param {string} fixturePath
+     * @param {string} fixtureName
+     * @param {Test} test
+     */
+    addTest(fixturePath, fixtureName, test) {
+        this.testRun.withSuite(fixturePath).withSuite(fixtureName).addTest(test);
+        this.testRun.computeStatus();
+    }
 
-  /**
-   * @param {Array<{ name: string, localPath: string }>} assets
-   */
-  addAssets(assets) {
-    this.assets.push(...assets);
-  }
+    /**
+     * @param {Array<{ name: string, localPath: string }>} assets
+     */
+    addAssets(assets) {
+        this.assets.push(...assets);
+    }
 }
 
 class Fixture {
-  /**
-   * @param {string} name - The fixture name
-   * @param {string} path - The path to the file defining the fixture
-   * @param {object} meta - Metadata about the fixture
-   */
-  constructor(name, path, meta) {
-    this.name = name;
-    this.path = path;
-    this.meta = meta;
     /**
-     * @type Map<string, BrowserTestRun>
+     * @param {string} name - The fixture name
+     * @param {string} path - The path to the file defining the fixture
+     * @param {object} meta - Metadata about the fixture
      */
-    this.browserTestRuns = new Map();
+    constructor(name, path, meta) {
+        this.name = name;
+        this.path = path;
+        this.meta = meta;
+        /**
+         * @type Map<string, BrowserTestRun>
+         */
+        this.browserTestRuns = new Map();
 
-    /**
-     * @type Date
-     */
-    this.startTime = null;
-    /**
-     * @type Date
-     */
-    this.endTime = null;
-  }
-
-  /**
-   * @param {string} userAgent
-   * @param {Test} test
-   * @param {Array<{ name: string, localPath: string }>} screenshotAssets
-   * @param {Array<{ name: string, localPath: string }>} videoAssets
-   */
-  addTestWithAssets(userAgent, test, screenshotAssets, videoAssets) {
-    if (!this.browserTestRuns.has(userAgent)) {
-      this.browserTestRuns.set(userAgent, new BrowserTestRun(userAgent));
+        /**
+         * @type Date | null
+         */
+        this.startTime = null;
+        /**
+         * @type Date | null
+         */
+        this.endTime = null;
     }
 
-    const tr = this.browserTestRuns.get(userAgent);
+    /**
+     * @param {string} userAgent
+     * @param {Test} test
+     * @param {Array<{ name: string, localPath: string }>} screenshotAssets
+     * @param {Array<{ name: string, localPath: string }>} videoAssets
+     */
+    addTestWithAssets(userAgent, test, screenshotAssets, videoAssets) {
+        if (!this.browserTestRuns.has(userAgent)) {
+            this.browserTestRuns.set(userAgent, new BrowserTestRun(userAgent));
+        }
 
-    for (const a of screenshotAssets) {
-      test.attach({
-        path: a.name,
-        name: a.name,
-        contentType: 'image/png',
-      });
+        const tr = this.browserTestRuns.get(userAgent);
+
+        for (const a of screenshotAssets) {
+            test.attach({
+                path: a.name,
+                name: a.name,
+                contentType: 'image/png',
+            });
+        }
+        for (const a of videoAssets) {
+            test.attach({
+                path: a.name,
+                name: a.name,
+                contentType: 'video/mp4',
+            });
+        }
+
+        // @ts-ignore
+        tr.addTest(this.path, this.name, test);
+        // @ts-ignore
+        tr.addAssets([...screenshotAssets, ...videoAssets]);
     }
-    for (const a of videoAssets) {
-      test.attach({
-        path: a.name,
-        name: a.name,
-        contentType: 'video/mp4',
-      });
+
+    collectTestRuns() {
+        return [...this.browserTestRuns.values()].map((bc) => bc.testRun);
     }
-
-    tr.addTest(this.path, this.name, test);
-    tr.addAssets([...screenshotAssets, ...videoAssets]);
-  }
-
-  collectTestRuns() {
-    return [...this.browserTestRuns.values()].map((bc) => bc.testRun);
-  }
 }
 
 module.exports = {
-  BrowserTestRun,
-  Fixture,
+    BrowserTestRun,
+    Fixture,
 };
