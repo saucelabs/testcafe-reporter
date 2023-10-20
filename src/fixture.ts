2628738bb8ac6105c10ee5e4041c015d04ae6289
Alex Plischke <alex.plischke@saucelabs.com>
2023-10-20 10:44:49 -0700

    style: format all code with prettier (#49)
    * style: format all code with prettier
    
    * ci: add codeowners
    
    * ci: add missing lint step
    
    * ci: use stable dep install

--- src/fixture.ts
+++ src/fixture.ts
@@ -1,103 +1,105 @@
 import { TestRun, Test } from '@saucelabs/sauce-json-reporter';
 
-export type Asset = { name: string, localPath: string };
+export type Asset = { name: string; localPath: string };
 export type Assets = Array<Asset>;
 
 /**
-* Provides a facade to help associate a sauce-json-reporter/TestRun with additional TestCafe specific
-* execution metadata (e.g. browser under test, generated assets).
-*/
-class BrowserTestRun {
-    userAgent: string;
-    testRun: TestRun;
-    assets: Assets;
+ * Provides a facade to help associate a sauce-json-reporter/TestRun with additional TestCafe specific
+ * execution metadata (e.g. browser under test, generated assets).
+ */
+export class BrowserTestRun {
+  userAgent: string;
+  testRun: TestRun;
+  assets: Assets;
 
-    #browser: string;
-    platform: string;
+  #browser: string;
+  platform: string;
 
-    constructor(userAgent: string) {
-        this.userAgent = userAgent;
-        this.testRun = new TestRun();
-        (this.testRun.metadata as { userAgent: string })['userAgent'] = userAgent;
-        this.assets = [];
+  constructor(userAgent: string) {
+    this.userAgent = userAgent;
+    this.testRun = new TestRun();
+    (this.testRun.metadata as { userAgent: string })['userAgent'] = userAgent;
+    this.assets = [];
 
-        const [ browser, platform ] = userAgent.split('/').map((ua) => ua.trim());
+    const [browser, platform] = userAgent.split('/').map((ua) => ua.trim());
 
-        this.#browser = browser;
-        this.platform = platform;
-    }
+    this.#browser = browser;
+    this.platform = platform;
+  }
 
-    get browserName() {
-        const [ name = 'unknown',  _ ] = this.#browser.split(' ');
-        return name;
-    }
+  get browserName() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [name = 'unknown', _] = this.#browser.split(' ');
+    return name;
+  }
 
-    get browserVersion() {
-        const [ _, version = 'unknown' ] = this.#browser.split(' ');
-        return version;
-    }
+  get browserVersion() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, version = 'unknown'] = this.#browser.split(' ');
+    return version;
+  }
 
-    get browser() {
-        return this.#browser;
-    }
+  get browser() {
+    return this.#browser;
+  }
 
-    addTest(fixturePath: string, fixtureName: string, test: Test) {
-        this.testRun.withSuite(fixturePath).withSuite(fixtureName).addTest(test);
-        this.testRun.computeStatus();
-    }
+  addTest(fixturePath: string, fixtureName: string, test: Test) {
+    this.testRun.withSuite(fixturePath).withSuite(fixtureName).addTest(test);
+    this.testRun.computeStatus();
+  }
 
-    addAssets(assets: Assets) {
-        this.assets.push(...assets);
-    }
+  addAssets(assets: Assets) {
+    this.assets.push(...assets);
+  }
 }
 
-class Fixture {
-    name: string;
-    path: string;
-    meta: object;
-    browserTestRuns: Map<string, BrowserTestRun>;
-    startTime?: Date;
-    endTime?: Date;
+export class Fixture {
+  name: string;
+  path: string;
+  meta: object;
+  browserTestRuns: Map<string, BrowserTestRun>;
+  startTime?: Date;
+  endTime?: Date;
 
-    constructor(name: string, path: string, meta: object) {
-        this.name = name;
-        this.path = path;
-        this.meta = meta;
-        this.browserTestRuns = new Map();
+  constructor(name: string, path: string, meta: object) {
+    this.name = name;
+    this.path = path;
+    this.meta = meta;
+    this.browserTestRuns = new Map();
+  }
+
+  addTestWithAssets(
+    userAgent: string,
+    test: Test,
+    screenshotAssets: Assets,
+    videoAssets: Assets,
+  ) {
+    if (!this.browserTestRuns.has(userAgent)) {
+      this.browserTestRuns.set(userAgent, new BrowserTestRun(userAgent));
     }
 
-    addTestWithAssets(userAgent: string, test: Test, screenshotAssets: Assets, videoAssets: Assets) {
-        if (!this.browserTestRuns.has(userAgent)) {
-            this.browserTestRuns.set(userAgent, new BrowserTestRun(userAgent));
-        }
+    const tr = this.browserTestRuns.get(userAgent) as BrowserTestRun;
 
-        const tr = this.browserTestRuns.get(userAgent) as BrowserTestRun;
-
-        for (const a of screenshotAssets) {
-            test.attach({
-                path: a.name,
-                name: a.name,
-                contentType: 'image/png',
-            });
-        }
-        for (const a of videoAssets) {
-            test.attach({
-                path: a.name,
-                name: a.name,
-                contentType: 'video/mp4',
-            });
-        }
-
-        tr.addTest(this.path, this.name, test);
-        tr.addAssets([...screenshotAssets, ...videoAssets]);
+    for (const a of screenshotAssets) {
+      test.attach({
+        path: a.name,
+        name: a.name,
+        contentType: 'image/png',
+      });
+    }
+    for (const a of videoAssets) {
+      test.attach({
+        path: a.name,
+        name: a.name,
+        contentType: 'video/mp4',
+      });
     }
 
-    collectTestRuns() {
-        return [...this.browserTestRuns.values()].map((bc) => bc.testRun);
-    }
+    tr.addTest(this.path, this.name, test);
+    tr.addAssets([...screenshotAssets, ...videoAssets]);
+  }
+
+  collectTestRuns() {
+    return [...this.browserTestRuns.values()].map((bc) => bc.testRun);
+  }
 }
-
-module.exports = {
-    BrowserTestRun,
-    Fixture,
-};
